c20dc984d4b6742449633bd36c540933154222d5
fabiankuenzer <fabiankuenzer@gmail.com>
2023-10-11 15:58:28 +0200

    Add simple loop functions for import

---
+++ loops.py
@@ -0,0 +1,12 @@
+def fly_right_loop(drone):
+    drone.move_forward(100)
+    drone.move_right(100)
+    drone.move_back(100)
+    drone.move_left(100)
+
+
+def fly_looping(drone):
+    drone.move_forward(100)
+    drone.move_up(100)
+    drone.move_back(100)
+    drone.move_down(100)
